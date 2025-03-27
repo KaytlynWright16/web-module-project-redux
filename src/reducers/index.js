bc5b410ed1916c75372664d4cfb7f6eba15d94fe
Kaytlyn Wright <kaytlynwright16@gmail.com>
2025-03-27 12:41:34 -0400

    halfway done

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,10 @@
 import { combineReducers } from 'redux';
 
 import movieReducer from './movieReducer';
+import {favoriteReducer} from './favoriteReducer'
+
+const rootReducer = conbineReducers({
+    movieReducer
+})
 
 export default movieReducer;
--- src/reducers/movieReducer.js
+++ src/reducers/movieReducer.js
@@ -1,16 +1,26 @@
 import { ADD_MOVIE, DELETE_MOVIE } from '../actions/movieActions.js';
 import movies from './../data.js';
 
+
 const initialState = {
     movies: movies,
     appTitle: "IMDB Movie Database"
 }
 
-const reducer = (state, action) => {
+const reducer = (state = initialState, action) => {
     switch(action.type) {
         case DELETE_MOVIE:
             return {
                 movies: state.movies.filter(item=>(action.payload !== item.id))
+            }
+        case ADD_MOVIE:
+            const newMovies = {
+                ...action.payload,
+                id: Date.now()
+            }
+            return {
+                ...state,
+                movies: [...state.movies, newMovie]
             }
         default:
             return state;
